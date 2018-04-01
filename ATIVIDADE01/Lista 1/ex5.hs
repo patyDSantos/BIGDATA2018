075bd40fa552ee28c1d450565dcab1af381ea989
Patrícia Santos <patriciadiassantos@gmail.com>
2018-04-01 18:14:19 -0300

    atualizar2

--- ATIVIDADE01/Lista 1/ex5.hs
+++ ATIVIDADE01/Lista 1/ex5.hs
@@ -1,4 +1,4 @@
-{-Exercício 04: Faça um programa que retorne True caso a entrada seja menor que -1 ou (maior que 1 E múltiplo de 2), e False caso contrário.-}
+{-Exercício 05: Faça um programa que retorne True caso a entrada seja menor que -1 ou (maior que 1 E múltiplo de 2), e False caso contrário.-}
 verify :: Integer -> Bool
 verify var = if var < -1 || var > 1 && mod var 2 == 0 then True else False
 
