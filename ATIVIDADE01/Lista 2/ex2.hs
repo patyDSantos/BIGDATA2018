a970ea3945e5619b247347b882596fea52dc3ef2
Patrícia Santos <patriciadiassantos@gmail.com>
2018-04-01 21:57:55 -0300

    atualiza2_lista2

---
+++ ATIVIDADE01/Lista 2/ex2.hs
@@ -0,0 +1,23 @@
+{-Exercício 2: Crie uma função tipoTriangulo que determina o tipo do triângulo formado pelos três lados x, y, z.-}
+
+ehTriangulo :: Double -> Double -> Double -> Bool
+ehTriangulo x y z
+    | abs(x-y) > z || z > (y*x) = False
+    | abs(y-z) > x || x > (y*z) = False
+    | abs(x-z) > y || y > (x*z) = False    
+    | otherwise = True
+
+
+tipoTriangulo :: Double -> Double -> Double -> String
+tipoTriangulo x y z
+    | ehTriangulo x y z == False = "Invalido. Estes valores nao formam um triangulo"
+    | (x==y) && (y==z) && (x==z) = "Triangulo Equilatero"
+    | (x/=y) && (y/=z) && (x/=z) = "Triangulo Escaleno"
+    | (x==y) || (x==z) || (y/=z) = "Triangulo Isosceles"
+        
+main :: IO ()
+main = do
+    putStrLn $ "Verificando: " ++ show (tipoTriangulo 7 9 8)
+    putStrLn $ "Verificando: " ++ show (tipoTriangulo 2 2 10)
+    putStrLn $ "Verificando: " ++ show (tipoTriangulo 2 2 2)
+    putStrLn $ "Verificando: " ++ show (tipoTriangulo 10 12 10)
