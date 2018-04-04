057a7a5d8f4baced1f12e0d516dffbd972309d7b
Patrícia Santos <patriciadiassantos@gmail.com>
2018-04-03 23:26:27 -0300

    ultimo

--- ATIVIDADE01/Lista 3/ex1.hs
+++ ATIVIDADE01/Lista 3/ex1.hs
@@ -1,13 +1,11 @@
 {-Exercício 1:  Crie uma função divisivel20 x que retorna verdadeiro se x for divisível por todos os números de 1 a 20.-}
 
-divisivel20 :: Integer -> String
-divisivel20 x = divisivel20' x 20
-  where
-    divisivel20' x i
-      | i == 1 = "Esse numero e divisivel por todos de 1 a 20"
-      | x `mod` i == 0 = divisivel20' x (i - 1)
-      | otherwise = "Esse numero nao e divisivel por todos de 1 a 20"
-main :: IO ()
-main = do
-    putStrLn $  "Resposta: " ++ show (divisivel20 1)
-    putStrLn $  "Resposta: " ++ show (divisivel20 41)
+divisivel20 :: Int -> Bool
+divisivel20 x = if length (filter (== True) var) == 20
+                then True
+                else False
+                where l = map (x `mod`) [1..20] 
+                      var =  map (== 0) l
+main = do 
+       print(divisivel20 400)
+       print(divisivel20 4400)
--- ATIVIDADE01/Lista 3/ex3.hs
+++ ATIVIDADE01/Lista 3/ex3.hs
@@ -1,1 +1,8 @@
 {-Exercício 3:  Crie a lista de números de Fibonacci utilizando uma função geradora.-}
+
+listaFib :: Integer -> [Integer]
+listaFib n = map (gera) [0..n]
+                where
+                gera x = round(((1/sqrt(5))*(((1+sqrt(5))/2)^x))-((1/sqrt(5))*(((1-sqrt(5))/2)^x)))
+main = do 
+       print(listaFib 30)
