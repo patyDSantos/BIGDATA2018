d025e3e2b88052a7467344de074f8b668ffd77e2
abelinda <patriciadiassantos@gmail.com>
2018-04-12 21:10:10 -0300

    corrige 1

--- ATIVIDADE03/ex3.hs
+++
@@ -1,9 +0,0 @@
-{-Exercício 3:  Crie a lista de números de Fibonacci utilizando uma função geradora.-}
-
-geradoraFib :: Integer -> [Integer]
-geradoraFib n = map (calcula) [0..n]
-                where
-                calcula x = round(((1/sqrt(5))*(((1+sqrt(5))/2)^x))-((1/sqrt(5))*(((1-sqrt(5))/2)^x)))
-				
-main = do 
-       print(geradoraFib 10)
