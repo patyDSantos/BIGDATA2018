108f9c7841086e93c5b7e261deac04b7b29f09a3
Patrícia Santos <patriciadiassantos@gmail.com>
2018-04-27 01:29:50 -0300

    atividade3

--- Desktop/BIGDATA2018-master/BIGDATA2018-master/ATIVIDADE01/Lista 1/ex12.hs
+++
@@ -1,9 +0,0 @@
-{-Exercício 12: Crie um concatenador de strings que concatena duas strings separadas por espaço.-}
-
-concatenador :: String -> String -> String
-concatenador x y = x ++ (' ' : y)
-
--- |'main' executa programa principal
-main :: IO ()
-main = do
-   putStrLn $ "Concatenador: " ++ show (concatenador "1234" "4321")
--- Desktop/BIGDATA2018-master/BIGDATA2018-master/ATIVIDADE01/Lista 3/ex5.hs
+++
@@ -1,7 +0,0 @@
-{-Exercício 5:  Faça uma função para calcular o produto escalar entre dois vetores.-}
-
-produtoEscalar :: [Int] -> [Int] -> Int
-produtoEscalar as var = sum (zipWith (*) as var)
-       
-main = do 
-       print(produtoEscalar [4,8] [4,12])
